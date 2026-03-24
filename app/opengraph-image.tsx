e5db0d91013811c22479fe008721afaa0c00f03f
PabloVallecillos <vallecillospablo@gmail.com>
2026-03-24 13:02:50 +0100

    feat: add OG image with red circle

---
+++ app/opengraph-image.tsx
@@ -0,0 +1,32 @@
+import { ImageResponse } from 'next/og'
+
+export const runtime = 'edge'
+export const size = { width: 1200, height: 630 }
+export const contentType = 'image/png'
+
+export default function Image() {
+  return new ImageResponse(
+    (
+      <div
+        style={{
+          background: '#000',
+          width: '100%',
+          height: '100%',
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+        }}
+      >
+        <div
+          style={{
+            width: 300,
+            height: 300,
+            borderRadius: '50%',
+            background: '#dc143c',
+          }}
+        />
+      </div>
+    ),
+    { ...size }
+  )
+}
